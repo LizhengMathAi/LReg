4862f863fd01e121c82d77d5dfe9bd7f287e99c3
LizhengMathAi <52631612+LizhengMathAi@users.noreply.github.com>
2025-06-16 19:57:53 +0800

    Add files via upload

---
+++ cifar_ablation_study/run.sh
@@ -0,0 +1,10 @@
+python cifar_ablation_study.py --dof 2 --data mnist --no-da 
+
+python cifar_ablation_study.py --dof 2 --data fashionmnist --no-da 
+python cifar_ablation_study.py --dof 2 --data fashionmnist
+
+python cifar_ablation_study.py --dof 2 --data cifar10 --no-da 
+python cifar_ablation_study.py --dof 2 --data cifar10
+
+python cifar_ablation_study.py --dof 2 --data cifar100 --no-da 
+python cifar_ablation_study.py --dof 2 --data cifar100
